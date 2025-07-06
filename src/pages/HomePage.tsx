3372afd80627d24c5f65a5fe00dc0ef1fc677abb
Vrzasq <jakub.romel@gmail.com>
2025-07-06 21:53:52 +0200

    use on pointer event for image enlargment

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -22,9 +22,7 @@ const HomePage = () => {
                     alt="Profile"
                     className={`w-1/5 aspect-square object-cover rounded-full border-4 border-white shadow-xl mb-10 object-center cursor-pointer transition-transform duration-300 ${isProfileLarge ? 'scale-' + scale + ' z-50' : ''}`}
                     style={{ objectPosition: 'center' }}
-                    onClick={() => setIsProfileLarge(!isProfileLarge)}
-                    onTouchEnd={(e) => { e.preventDefault(); setIsProfileLarge(!isProfileLarge); }}
-                    onTouchStart={e => e.preventDefault()}
+                    onPointerUp={() => setIsProfileLarge(!isProfileLarge) }                 
                 />
                 <div className="space-y-6">
                     <a
